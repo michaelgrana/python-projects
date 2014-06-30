eb80f1bdabd3d5a325a2fbf8796f48f15ad1b82a
michaelgrana <granamike@live.com>
2014-06-30 16:54:18 -0400

    added basic value types

---
+++ python file.py
@@ -0,0 +1,17 @@
+answer = input ("what is your name?")
+print("hello" + answer)
+
+answer = input ("what is your age?")
+
+print("age: " + answer)
+
+#boolean example 
+
+answer = input ("are you happy?")
+
+print("happy: " + answer)
+
+#inpute() example
+
+answer = input ("what is your age?")
+print(answer)
